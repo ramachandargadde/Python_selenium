1c1ba4dfb4170f940e1a1c473133caa1f31c5261
Ramachandar Gadde <29334827+ramachandargadde@users.noreply.github.com>
2021-01-29 10:07:32 +0530

    Add files via upload

---
+++ 12345.py
@@ -0,0 +1,21 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
+import time
+#time.sleep(1000000)
+baseurl="www.google.com"
+#baseurl1="https://10.104.119.201"
+driver = webdriver.Chrome(executable_path="C:\\Users\\ramacgad\\Desktop\\software updates\\3.7\\chromedriver.exe")
+driver.get(baseurl)
+#driver.find_element(By.ID,"details-button").click()
+#driver.find_element(By.ID,"proceed-link").click()
+driver.find_element(By.ID,"label_username").send_keys("15j41a0441@gmail.com")
+driver.find_element(By.ID,"label_password").send_keys("9963626210@v")
+driver.find_element(By.ID,"loginPage_LoginButton_label").click()
+time.sleep(10)
+#delay=3
+#dnacurl=baseurl+"/webacs/loginAction.do?action=login&product=wcs&selectedCategory=en&#pageId=dnac_integration_page_id"
+#driver.execute_script("window.open('" +dnacurl + "');")
---
+++ chandu.py
@@ -0,0 +1,19 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+
+baseUrl = "https://10.197.72.90"
+driver = webdriver.Chrome(executable_path="C:\\Users\\ramacgad\\Desktop\\software updates\\3.7\\chromedriver.exe")
+driver.get(baseUrl)
+try:
+element = WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.ID, "id-of-new-element"))
+)
+finally:
+username=driver.find_element_by_id("label_username")
+password=driver.find_element_by_id("label_password")
+username.send_keys("root")
+password.send_keys("roZes123")
+driver.quit()
