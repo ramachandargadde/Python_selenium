66c05ea8ef5e708a2bfa09fa5ee7dc7be95f6d0e
Ramachandar Gadde <29334827+ramachandargadde@users.noreply.github.com>
2021-01-29 10:24:47 +0530

    Update ui_server_login.py

--- ui_server_login.py
+++ ui_server_login.py
@@ -1,10 +1,10 @@
 import selenium
 import webbrowser
 import time
-webbrowser.open('https://10.197.72.90')
+webbrowser.open('https://10.197.xx.xx')
 time.sleep( 5 )
 username = selenium.find_element_by_name("username")
 password = selenium.find_element_by_name("password")
 username.send_keys("root")
-password.send_keys("roZes123")
+password.send_keys("xxxxxxxx")
 selenium.find_element("#xwt_widget_uishell_Header_0 > div > div.headerLeft > div.dijitInline.toggleNode.isOpen").click()
